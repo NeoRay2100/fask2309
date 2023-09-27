663769457754a7b9e049d327cd11e57df5a7e811
neroray2100 <1792438208@qq.com>
2023-09-27 15:35:25 +0800

    movie page

---
+++ .history/app_u1_20230927143514.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route("/user/<name>")
+def u1(name):
+    return f"{name}"
+
+
+from markupsafe import escape
+
+
+@app.route("/user2/<name>")
+def u2(name):
+    return f"User: {escape(name)}"
